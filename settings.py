de2c65ad222966708e31a11e85e53727f13c33bf
ayaMagdy23 <Aya2108429@miuegypt.edu.eg>
2024-11-15 20:28:50 +0200

    push

--- settings.py
+++ settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+from decouple import config
 
 from pathlib import Path
 
@@ -73,16 +74,16 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'my_database_name',
-        'USER': 'postgres',
-        'PASSWORD': 'devops123',  # Enter the updated password here
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'my_database_name',
+#         'USER': 'postgres',
+#         'PASSWORD': 'devops123',  # Enter the updated password here
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 
